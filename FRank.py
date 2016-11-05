1ebc45bee083e96d19ebab7f171b7bf57c4e565e
Xingjia Zhang <xzhang009@e.ntu.edu.sg>
2016-11-05 23:32:20 +0800

    Added impl for lower/upper bounds and C

--- FRank.py
+++ FRank.py
@@ -13,18 +13,19 @@ def fRank(k, adjList, alpha):
 #        if i != 0:
 #            R = computeR(C, G)
 #            G = computeG(R)
+        prevR = np.copy(r)
         r = computeProbabilityOfILengthRandomWalk(G, i, e)
-        lowerBounds = equation7(N, alpha, lowerBounds, r)
-        upperBounds = equation8(N, alpha, upperBounds, r, G)
-#        epsi = computeEpsi(C, lowerBounds)
-#        C = computeC(epsi, C, upperBounds)
+        lowerBounds = equation7(N, alpha, lowerBounds, r, i)
+        upperBounds = equation8(N, alpha, upperBounds, r, prevR, G, i)
+       epsi = computeEpsi(C, lowerBounds, k)
+       C = computeC(epsi, C, upperBounds)
         i = i + 1
     return C
 
 def getAdjMatrix(adjList):
     adjMat = np.zeros((len(adjList), len(adjList)))
 
-    for u, vs in adjList.iteritems():
+    for u, us in adjList.iteritems():
         for v in vs:
             adjMat[u][v] = 1
     return adjMat
@@ -32,8 +33,27 @@ def getAdjMatrix(adjList):
 def computeProbabilityOfILengthRandomWalk(W, i, e):
     return np.linalg.matrix_power(W, i).dot(e)
 
-def equation7(N, alpha, prevLowerBounds, r):
-    pass
+def equation7(N, alpha, prevLowerBounds, r, i):
+    if i == 0:
+        return np.ones(N) * (1 - alpha)/N
+    else:
+        return np.add(prevLowerBounds, (1 - alpha) * math.pow(alpha, i) * r)
 
-def equation8(N, alpha, prevLowerBounds, r, W):
-    pass
+def equation8(N, alpha, prevLowerBounds, r, prevR, W, i):
+    Wbar = np.amax(W, axis=1) # max for each row, need to confirm with you
+    if i == 0:
+        return np.ones(N) * alpha/(1 - alpha) * Wbar
+    else:
+        bigDelta = np.sum(np.substract(r, prevR))
+        delta = math.pow(alpha, i+1) * r
+        return np.add(prevLowerBounds, math.pow(alpha, i) * r, bigDelta * delta * Wbar)
+
+# These 2 are for eqn 6
+def computeEpsi(C, lowerBounds, k):
+    kthHighest = lowerBounds.sort()[k]
+    return kthHighest
+
+def computeC(epsi, C, upperBounds):
+    mask = upperBounds > epsi
+    C = [elm for (idx, elm) in C if mask[idx]]
+    return C
