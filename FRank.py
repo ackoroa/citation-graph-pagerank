7b959dfdc7038bd98d1f9d8ca68f87071faf1a6d
Arnold Koroa <arnold.koroa@gmail.com>
2016-11-07 00:01:27 +0800

    Completed most of the implementation

--- FRank.py
+++ FRank.py
@@ -1,37 +1,75 @@
 import numpy as np
+from collections import deque
 
 def fRank(k, adjList, alpha):
     i = 0
-    N = len(adjList)
     C = adjList.keys()
-    G = getAdjMatrix(adjList)
-    e = np.ones(N) * 1.0/len(N)
 
-    lowerBounds = np.zeros(N)
-    upperBounds = np.ones(N)
+    seqId = 0
+    idToSeq = {}
+    seqToId = {}
+    for u in adjList.iteritems():
+        idToSeq[u] = seqId
+        seqToId[seqId] = u
+        seqId = seqId + 1
+    G = getAdjMatrix(adjList, idToSeq)
+    revSeqAdjList = getReverseSeqAdjList(adjList, idToSeq)
+
+    lowerBounds = np.zeros(len(G))
+    upperBounds = np.ones(len(G))
+    mask = np.ones(len(G), dtype=bool)
     while len(C) > k:
-#        if i != 0:
-#            R = computeR(C, G)
-#            G = computeG(R)
-        prevR = np.copy(r)
-        r = computeProbabilityOfILengthRandomWalk(G, i, e)
-        lowerBounds = equation7(N, alpha, lowerBounds, r, i)
-        upperBounds = equation8(N, alpha, upperBounds, r, prevR, G, i)
-       epsi = computeEpsi(C, lowerBounds, k)
-       C = computeC(epsi, C, upperBounds)
+        if i != 0:
+            R = computeReachableNodes(C, revAdjList)
+            G = G[R][:,R]
+        prevR = np.copy(r)[mask]
+        r = np.linalg.matrix_power(G, i).dot(np.ones(len(G)) * 1.0/len(G))
+        lowerBounds = equation7(len(G), alpha, lowerBounds[mask], r, i)
+        upperBounds = equation8(len(G), alpha, upperBounds[mask], r, prevR, G, i)
+        eps_i = np.copy(lowerBounds).sort()[k]
+        mask = upperBounds >= eps_i
+        C = C[mask]
         i = i + 1
-    return C
 
-def getAdjMatrix(adjList):
+    #This is wrong. After thr prunings the indices in the matrix 
+    #no longer correspond to the initial indices
+    topKNodes = []
+    for c in C:
+        topKNodes.append(seqToId[c])
+    return topKNodes
+
+def getAdjMatrix(adjList, idToSeq):
     adjMat = np.zeros((len(adjList), len(adjList)))
-
-    for u, us in adjList.iteritems():
+    for u, vs in adjList.iteritems():
+        uSeq = idToSeq[u]
         for v in vs:
-            adjMat[u][v] = 1
+            vSeq = idToSeq[v]
+            adjMat[uSeq][vSeq] = 1
     return adjMat
 
-def computeProbabilityOfILengthRandomWalk(W, i, e):
-    return np.linalg.matrix_power(W, i).dot(e)
+def getReverseSeqAdjList(adjList, idToSeq):
+    revAdjList = {}
+    for u, vs in adjList.iteritems():
+        uSeq = idToSeq[u]
+        for v in vs:
+            vSeq = idToSeq[v]
+            if not vSeq in revAdjList:
+                revAdjList[vSeq] = []
+            revAdjList[vSeq].append(uSeq)
+    return revAdjList    
+
+def computeReachableNodes(C, revSeqAdjList):
+    R = []
+    queue = deque(C)
+    visited = set()
+    while len(queue) > 0:
+        u = queue.popLeft()
+        visited.add(u)
+        for v in revSeqAdjList[u]:
+            if v not in visited:
+                queue.append(v)
+                R.append(v)
+    return R
 
 def equation7(N, alpha, prevLowerBounds, r, i):
     if i == 0:
@@ -39,21 +77,11 @@ def equation7(N, alpha, prevLowerBounds, r, i):
     else:
         return np.add(prevLowerBounds, (1 - alpha) * math.pow(alpha, i) * r)
 
-def equation8(N, alpha, prevLowerBounds, r, prevR, W, i):
-    Wbar = np.amax(W, axis=1) # max for each row, need to confirm with you
+def equation8(N, alpha, prevUpperBounds, r, prevR, W, i):
+    Wbar = np.amax(W, axis=1) # max for each row
     if i == 0:
         return np.ones(N) * alpha/(1 - alpha) * Wbar
     else:
         bigDelta = np.sum(np.substract(r, prevR))
         delta = math.pow(alpha, i+1) * r
-        return np.add(prevLowerBounds, math.pow(alpha, i) * r, bigDelta * delta * Wbar)
-
-# These 2 are for eqn 6
-def computeEpsi(C, lowerBounds, k):
-    kthHighest = lowerBounds.sort()[k]
-    return kthHighest
-
-def computeC(epsi, C, upperBounds):
-    mask = upperBounds > epsi
-    C = [elm for (idx, elm) in C if mask[idx]]
-    return C
+        return np.add(prevUpperBounds, math.pow(alpha, i) * r, bigDelta * delta * Wbar)
